1815aacb4bf8a89e40b0297c647ea82dae4091e3
Takuya Morizumi <tmtm3980@gmail.com>
2020-02-06 17:13:26 +0900

    answer 05_class_definition/simple_model

--- 05_class_definition/simple_model.rb
+++ 05_class_definition/simple_model.rb
@@ -12,3 +12,55 @@
 # 2. initializeメソッドはハッシュを受け取り、attr_accessorで作成したアトリビュートと同名のキーがあれば、自動でインスタンス変数に記録する
 #   1. ただし、この動作をwriterメソッドの履歴に残してはいけない
 # 3. 履歴がある場合、すべての操作履歴を放棄し、値も初期状態に戻す `restore!` メソッドを作成する
+
+module SimpleModel
+  def initialize(**hash)
+    hash.each do |key, value|
+      if self.class.instance_methods.include?("#{key}=".to_sym)
+        instance_variable_set("@#{key}", value) 
+        instance_variable_set("@#{key}_initial_value", value) 
+      end
+    end
+  end
+
+  def self.included(base)
+    base.instance_eval do
+      def self.attr_accessor(*keys)
+        instance_variable_set(:@attr_accessor_keys, keys)
+
+        keys.each do |key|
+          attr_reader key
+
+          define_method("#{key}=") do |value|
+            history = instance_variable_get("@#{key}_history") || []
+
+            instance_variable_set("@#{key}_history", history.push(value))
+            instance_variable_set("@#{key}", value)
+          end
+
+          define_method("#{key}_changed?") do
+            return false if instance_variable_get("@#{key}_history").nil?
+
+            !instance_variable_get("@#{key}_history").empty?
+          end
+        end
+      end
+    end
+  end
+
+  def changed?
+    changed_method_symbols = self.class.instance_methods.select { |v| v.to_s.end_with?("_changed?") }
+    changed_method_symbols.any? { |h| send(h) }
+  end
+
+  def restore!
+    keys = self.class.instance_variable_get(:@attr_accessor_keys)
+
+    return if keys.nil?
+
+    keys.each do |key|
+      instance_variable_set("@#{key}_history", [])
+      instance_variable_set("@#{key}", instance_variable_get("@#{key}_initial_value"))
+    end
+  end
+end
